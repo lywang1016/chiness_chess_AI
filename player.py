dda15f820c10849aa35212756331852b1a2263d2
lywang1016 <lywang1016@hotmail.com>
2022-05-26 11:46:24 -0700

    test

--- player.py
+++ player.py
@@ -63,33 +63,3 @@ class HumanPlayer:
     def take_action(self, posi):
         if self.current_piece_value != 0:
             return self.action_valid(self.candidate_move, posi)
-            # value = abs(self.current_piece_value)
-            # if value == 1:
-            #     rook = Rook(self.color, self.current_piece_posi)
-            #     candidate_move = rook.next_valid_move(self.current_board)
-            #     return self.action_valid(candidate_move, posi)
-            # if value == 2:
-            #     knight = Knight(self.color, self.current_piece_posi)
-            #     candidate_move = knight.next_valid_move(self.current_board)
-            #     return self.action_valid(candidate_move, posi)
-            # if value == 3:
-            #     cannon = Cannon(self.color, self.current_piece_posi)
-            #     candidate_move = cannon.next_valid_move(self.current_board)
-            #     return self.action_valid(candidate_move, posi)
-            # if value == 4:
-            #     minister = Minister(self.color, self.current_piece_posi)
-            #     candidate_move = minister.next_valid_move(self.current_board)
-            #     return self.action_valid(candidate_move, posi)
-            # if value == 5:
-            #     warrior = Warrior(self.color, self.current_piece_posi)
-            #     candidate_move = warrior.next_valid_move(self.current_board)
-            #     return self.action_valid(candidate_move, posi)
-            # if value == 6:
-            #     pawn = Pawn(self.color, self.current_piece_posi)
-            #     candidate_move = pawn.next_valid_move(self.current_board)
-            #     return self.action_valid(candidate_move, posi)
-            # if value == 7:
-            #     king = King(self.color, self.current_piece_posi)
-            #     candidate_move = king.next_valid_move(self.current_board)
-            #     return self.action_valid(candidate_move, posi)
-
