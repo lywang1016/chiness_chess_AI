7077f1639898ec9ecc24be3969e411a63205f7bf
lywang1016 <lywang1016@hotmail.com>
2022-05-29 22:10:12 -0700

    img

--- collect_data.py
+++ collect_data.py
@@ -3,9 +3,9 @@ from game import Game
 def main():
     # game = Game(r_type='human', b_type='human', if_record=False)
     # game = Game(r_type='ai', b_type='ai', if_record=True, if_gui=False, gui_update=0.5)
-    game = Game(r_type='human', b_type='ai', if_record=True)
+    game = Game(r_type='human', b_type='ai', if_record=False)
     # game = Game(r_type='ai', b_type='human', if_record=False)
-    for i in range(100):
+    for i in range(10):
         game.episode()
 
 if __name__ == '__main__':
