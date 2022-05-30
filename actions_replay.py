f8143eff8c183cb20be5b71542208ab38d9c7abd
lywang1016 <lywang1016@hotmail.com>
2022-05-29 21:59:31 -0700

    action replay

---
+++ actions_replay.py
@@ -0,0 +1,42 @@
+import csv
+import time
+from board import ChessBoard
+from display import GUI
+
+def actions_replay(csv_path):
+    chess_board = ChessBoard(record=False)
+    gui_update = 1.0
+    gui = GUI()
+    red = True
+    with open(csv_path, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ')
+        for row in reader:
+            if red:
+                gui.update(chess_board.board_states(), 'r')
+            else:
+                gui.update(chess_board.board_states(), 'b')
+            time.sleep(gui_update)
+            gui.check_event()
+
+            posi = (int(row[0][0]), int(row[0][2]))
+            if row[0][5] == ',':
+                move0 = int(row[0][4])
+                move1 = int(row[0][6:])
+            else:
+                move0 = int(row[0][4:6])
+                move1 = int(row[0][7:])
+            move = (move0, move1)
+            
+            chess_board.move_piece(posi, move)
+            red = not red
+
+    while True:
+        if red:
+            gui.update(chess_board.board_states(), 'r')
+        else:
+            gui.update(chess_board.board_states(), 'b')
+        time.sleep(gui_update)
+        gui.check_event()
+
+if __name__ == '__main__':
+    actions_replay('game_record/2022_5_29-21_36_30_529864.csv')
--- collect_data.py
+++ collect_data.py
@@ -3,8 +3,8 @@ from game import Game
 def main():
     # game = Game(r_type='human', b_type='human', if_record=False)
     # game = Game(r_type='ai', b_type='ai', if_record=True, if_gui=False, gui_update=0.5)
-    # game = Game(r_type='human', b_type='ai', if_record=False)
-    game = Game(r_type='ai', b_type='human', if_record=False)
+    game = Game(r_type='human', b_type='ai', if_record=True)
+    # game = Game(r_type='ai', b_type='human', if_record=False)
     for i in range(100):
         game.episode()
 
