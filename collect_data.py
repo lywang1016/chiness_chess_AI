f1917837cdc23b73dc251046321645cbbe9a7694
lywang1016 <lywang1016@hotmail.com>
2022-06-01 21:58:16 -0700

    ai_explore_rate

--- collect_data.py
+++ collect_data.py
@@ -5,14 +5,14 @@ def main():
     dataset = h5py_to_dataset('dataset/x.hdf5', 'dataset/y.hdf5')
 
     # game = Game(r_type='human', b_type='human', if_record=False, if_dataset=True)
-    # game = Game(r_type='ai', b_type='ai', if_record=True, if_dataset=True, if_gui=False, gui_update=0.5)
-    game = Game(r_type='human', b_type='ai', if_record=False, if_dataset=True, ai_explore_rate=0.05)
-    # game = Game(r_type='ai', b_type='human', if_record=True, if_dataset=True)
+    # game = Game(r_type='ai', b_type='ai', if_record=True, if_dataset=True, if_gui=False, , ai_explore_rate=0.05)
+    # game = Game(r_type='human', b_type='ai', if_record=False, if_dataset=True, ai_explore_rate=0.05)
+    game = Game(r_type='ai', b_type='human', if_record=False, if_dataset=False, ai_explore_rate=0.05)
 
-    for i in range(10):
+    for i in range(1):
         game.episode()
         merge_dataset(dataset, game.chess_board.dataset)
-        
+
     h5py_add_dataset('dataset/x.hdf5', 'dataset/y.hdf5', dataset)
 
 if __name__ == '__main__':
